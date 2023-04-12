af3ec0b6bd6834e1ed1ccd719f5bc9a1fdc8b41f
BagretsovDm <bagretsovdmitry@gmail.com>
2023-04-12 11:50:12 +0200

    добавление разметки

---
+++ app/src/main/java/com/example/rvexample/ExampleFragment.kt
@@ -0,0 +1,5 @@
+package com.example.rvexample
+
+import androidx.fragment.app.Fragment
+
+class ExampleFragment : Fragment(R.layout.fragment_example)
---
+++ app/src/main/java/com/example/rvexample/FragmentViewModel.kt
@@ -0,0 +1,5 @@
+package com.example.rvexample
+
+import androidx.lifecycle.ViewModel
+
+class FragmentViewModel : ViewModel()
